91474c3d948f27b37bc9e4f81c6096bcfa5d8b0d
Yoav Isaacs <yoav.isaacs@gmail.com>
2024-09-28 00:53:19 +0300

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,13 @@
-from pydantic.v1 import BaseSettings
+from pydantic.v1 import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    MONGODB_URI: str
-    MONGODB_NAME: str = "collaborative_coding_db"
+    MONGODB_URI: str = Field(..., env="MONGODB_URI")
+    MONGODB_NAME: str = Field(default="collaborative_coding_db", env="MONGO_DB_NAME")
     JWT_SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
+    class Config:
+        env_file = "../../.env"
+
+settings = Settings()
